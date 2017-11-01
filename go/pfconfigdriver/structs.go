8461f59cb35271b12d62eb7a1b893d5f5154f65a
Durand Fabrice <fdurand@inverse.ca>
2017-11-01 10:48:50 -0400

    Added new pfconfig structs

--- go/pfconfigdriver/structs.go
+++ go/pfconfigdriver/structs.go
@@ -91,6 +91,19 @@ type PfConfCaptivePortal struct {
 	WisprRedirection        string   `json:"wispr_redirection"`
 }
 
+type PfConfWebservices struct {
+	StructConfig
+	PfconfigMethod string `val:"hash_element"`
+	PfconfigNS     string `val:"config::Pf"`
+	PfconfigHashNS string `val:"webservices"`
+	Pass           string `json:"pass"`
+	Proto          string `json:"proto"`
+	User           string `json:"user"`
+	Port           string `json:"port"`
+	AAAPort        string `json:"aaa_port"`
+	Host           string `json:"host"`
+}
+
 type ManagementNetwork struct {
 	StructConfig
 	PfconfigMethod string `val:"element"`
@@ -99,6 +112,14 @@ type ManagementNetwork struct {
 	Vip            string `json:"vip"`
 	Mask           string `json:"mask"`
 	Int            string `json:"int"`
+}
+
+type PfClusterIp struct {
+	StructConfig
+	PfconfigMethod string `val:"hash_element"`
+	PfconfigNS     string `val:"resource::cluster_hosts_ip"`
+	PfconfigHashNS string `val:"-"`
+	Ip             string `json:"ip"`
 }
 
 func (mn *ManagementNetwork) GetNetIP(ctx context.Context) (net.IP, *net.IPNet, error) {
@@ -133,7 +154,76 @@ type configStruct struct {
 		General       PfConfGeneral
 		Fencing       PfConfFencing
 		CaptivePortal PfConfCaptivePortal
+                Webservices   PfConfWebservices
 	}
 }
 
 var Config configStruct
+
+type PfNetwork struct {
+	StructConfig
+	PfconfigMethod string `val:"keys"`
+	PfconfigNS     string `val:"config::Network"`
+	Keys           []string
+}
+
+type NetworkConf struct {
+	StructConfig
+	PfconfigMethod       string `val:"hash_element"`
+	PfconfigNS           string `val:"config::Network"`
+	PfconfigHashNS       string `val:"-"`
+	Dns                  string `json:"dns"`
+	DhcpStart            string `json:"dhcp_start"`
+	Gateway              string `json:"gateway"`
+	DomainName           string `json:"domain-name"`
+	NatEnabled           string `json:"nat_enabled"`
+	DhcpMaxLeaseTime     string `json:"dhcp_max_lease_time"`
+	Named                string `json:"named"`
+	FakeMacEnabled       string `json:"fake_mac_enabled"`
+	Dhcpd                string `json:"dhcpd"`
+	DhcpEnd              string `json:"dhcp_end"`
+	Type                 string `json:"type"`
+	Netmask              string `json:"netmask"`
+	DhcpDefaultLeaseTime string `json:"dhcp_default_lease_time"`
+	NextHop              string `json:"next_hop"`
+	SplitNetwork         string `json:"split_network"`
+}
+
+type PfRoles struct {
+	StructConfig
+	PfconfigMethod string `val:"keys"`
+	PfconfigNS     string `val:"config::Roles"`
+	Keys           []string
+}
+
+type RolesConf struct {
+	StructConfig
+	PfconfigMethod string `val:"hash_element"`
+	PfconfigNS     string `val:"config::Roles"`
+	PfconfigHashNS string `val:"-"`
+	Notes          string `json:"notes"`
+	MaxNodesPerPid string `json:"max_nodes_per_pid"`
+}
+
+type PfconfigDatabase struct {
+	StructConfig
+	PfconfigMethod string `val:"hash_element"`
+	PfconfigNS     string `val:"config::Pf"`
+	PfconfigHashNS string `val:"database"`
+	DBUser         string `json:"user"`
+	DBPassword     string `json:"pass"`
+	DBHost         string `json:"host"`
+	DBName         string `json:"db"`
+	DBPort         string `json:"port"`
+}
+
+type NetInterface struct {
+	StructConfig
+	PfconfigMethod string `val:"hash_element"`
+	PfconfigNS     string `val:"-"`
+	PfconfigHashNS string `val:"-"`
+	Ip             string `json:"ip"`
+	Type           string `json:"type"`
+	Enforcement    string `json:"enforcement"`
+	Mask           string `json:"mask"`
+}
